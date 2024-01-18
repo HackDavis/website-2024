8dde4311b78c9e59d5f96f684d0deb809e50824a
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2024-01-17 17:26:16 -0800

    feat: added basic elements of the footer, added images

--- app/(pages)/_components/Footer/Footer.tsx
+++ app/(pages)/_components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+'use client';
 import styles from './Footer.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
@@ -8,72 +9,95 @@ import {
   faInstagram,
   faDiscord,
 } from '@fortawesome/free-brands-svg-icons';
-
 import Image from 'next/image';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 export default function Footer() {
   return (
     <footer className={styles.container}>
-      <Image
-        src="hdLogoWhite.svg"
-        alt="hackdavis Logo"
-        width={103}
-        height={62}
-        className={styles.hdLogoWhite}
-      />
-      <div className={styles.brandIcons}>
-        <a className={styles.singleIcon} href="mailto:team@hackdavis.io">
-          <FontAwesomeIcon icon={faEnvelope} />
-        </a>
-        <a
-          className={styles.singleIcon}
-          href="https://medium.com/@HackDavis"
-          target="#"
-          rel="noopener noreferrer"
-          aria-label="@HackDavis on Medium"
-        >
-          <FontAwesomeIcon icon={faMedium} />
-        </a>
-        <a
-          className={styles.fbIcon}
-          href="https://www.facebook.com/HackDavis"
-          target="#"
-          rel="noopener noreferrer"
-          aria-label="HackDavis on Facebook"
-        >
-          <FontAwesomeIcon icon={faFacebookF} />
-        </a>
-        <a
-          className={styles.singleIcon}
-          href="https://twitter.com/hack_davis"
-          target="#"
-          rel="noopener noreferrer"
-          aria-label="@hack_davis on Twitter"
-        >
-          <FontAwesomeIcon icon={faTwitter} />
-        </a>
-        <a
-          className={styles.singleIcon}
-          href="https://www.instagram.com/hackdavis"
-          target="#"
-          rel="noopener noreferrer"
-          aria-label="@hackdavis on Instagram"
-        >
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
-        <a
-          className={styles.singleIcon}
-          href="https://discord.gg/wc6QQEc"
-          target="#"
-          rel="noopener noreferrer"
-          aria-label="HackDavis Discord server"
-        >
-          <FontAwesomeIcon icon={faDiscord} />
-        </a>
+      <div className={styles.hackdavisLogo}>
+        <Image
+          src="/Footer/hdLogoWhite.svg"
+          alt="hackdavis Logo"
+          width={92.5}
+          height={92.7}
+          className={styles.hdLogoWhite}
+        />
+
+        <Image
+          src="/Footer/hdLogoMotto.svg"
+          alt="hackdavis name and motto"
+          width={226}
+          height={71.55}
+          className={styles.hdLogoWhite}
+        />
       </div>
-      <p className={styles.copyright}>
-        &copy; 2024 HackDavis • Made with ☕️ & 💛 in Davis
-      </p>
+      <div className={styles.socialContent}>
+        <div className={styles.brandIcons}>
+          <a className={styles.singleIcon} href="mailto:team@hackdavis.io">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+          <a
+            className={styles.singleIcon}
+            href="https://medium.com/@HackDavis"
+            target="#"
+            rel="noopener noreferrer"
+            aria-label="@HackDavis on Medium"
+          >
+            <FontAwesomeIcon icon={faMedium} />
+          </a>
+          <a
+            className={styles.fbIcon}
+            href="https://www.facebook.com/HackDavis"
+            target="#"
+            rel="noopener noreferrer"
+            aria-label="HackDavis on Facebook"
+          >
+            <FontAwesomeIcon icon={faFacebookF} />
+          </a>
+          <a
+            className={styles.singleIcon}
+            href="https://twitter.com/hack_davis"
+            target="#"
+            rel="noopener noreferrer"
+            aria-label="@hack_davis on Twitter"
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <a
+            className={styles.singleIcon}
+            href="https://www.instagram.com/hackdavis"
+            target="#"
+            rel="noopener noreferrer"
+            aria-label="@hackdavis on Instagram"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a
+            className={styles.singleIcon}
+            href="https://discord.gg/wc6QQEc"
+            target="#"
+            rel="noopener noreferrer"
+            aria-label="HackDavis Discord server"
+          >
+            <FontAwesomeIcon icon={faDiscord} />
+          </a>
+        </div>
+        <p className={styles.copyright}>
+          &copy; 2024 HackDavis • Made with ☕️ & 💛 in Davis
+        </p>
+      </div>
+      <div className={styles.arrowToTop}>
+        <p>BACK TO TOP</p>
+        <Image
+          src="/Footer/arrowUp.svg"
+          alt="hackdavis name and motto"
+          width={24}
+          height={24}
+          className={styles.hdLogoWhite}
+        />
+      </div>
     </footer>
   );
 }
