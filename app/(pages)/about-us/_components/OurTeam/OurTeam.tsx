24aa3a544ba22135526f743a21283fbc68d3ef9c
Win Cheng <winzamark12@gmail.com>
2024-01-18 10:36:20 -0800

    Imported ProximaNova Fonts

--- app/(pages)/about-us/_components/OurTeam/OurTeam.tsx
+++ app/(pages)/about-us/_components/OurTeam/OurTeam.tsx
@@ -1,5 +1,14 @@
-// import styles from './OurTeam.module.scss';
+import styles from './OurTeam.module.scss';
 
 export default function OurTeam() {
-  return <div>hi</div>;
+  return (
+    <div className={styles.ourTeam}>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <h2>Get to know our team</h2>
+          <p>HackDavis is built by students, for students.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
