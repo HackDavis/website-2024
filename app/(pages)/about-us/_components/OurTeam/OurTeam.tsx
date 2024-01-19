76cc613e97ecf76ca04ae9f62c1e78818846b21f
Win Cheng <winzamark12@gmail.com>
2024-01-19 13:14:28 -0800

    Our Team component load profile images from postgresql complete

--- app/(pages)/about-us/_components/OurTeam/OurTeam.tsx
+++ app/(pages)/about-us/_components/OurTeam/OurTeam.tsx
@@ -1,7 +1,11 @@
 import ProfileCard from './_components/ProfileCard';
 import styles from './OurTeam.module.scss';
 
-export default function OurTeam() {
+import { prisma } from '@/lib/prisma';
+
+export default async function OurTeam() {
+  const team_members = await prisma.teamMember.findMany();
+
   return (
     <div className={styles.ourTeam}>
       <div className={styles.container}>
@@ -9,7 +13,20 @@ export default function OurTeam() {
           <h1>Get to know our team</h1>
           <p>HackDavis is built by students, for students.</p>
         </div>
-        <ProfileCard name="Amanda" title="Co-Director" />
+        <div className={styles.team}>
+          {team_members.map((member) => {
+            return (
+              <ProfileCard
+                key={member.id}
+                name={member.name}
+                title={member.position}
+                imageUrl={member.profileImageUrl}
+              ></ProfileCard>
+            );
+          })}
+          ;
+        </div>
+        {/* <ProfileCard name="Amanda" title="Co-Director" /> */}
       </div>
     </div>
   );
