8e1e6486ffa9f40c4ef6da71655dfc661687a348
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2024-01-19 19:04:23 -0800

    feat: implemented basic scroll with 7 folders

--- app/(pages)/about-us/_components/Archive/Archive.tsx
+++ app/(pages)/about-us/_components/Archive/Archive.tsx
@@ -1,5 +1,76 @@
-// import styles from './Archive.module.scss';
+'use client';
+import styles from './Archive.module.scss';
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Archive() {
-  return <div>hi</div>;
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const moveLeft = () => {
+    setScrollPosition(scrollPosition - 127.57); // max is 893
+  };
+
+  const startScrool = 290;
+
+  return (
+    <div className={styles.container} onClick={moveLeft}>
+      <div>
+        <Image
+          className={styles.folder}
+          src="/archive/2023.png"
+          alt="thing"
+          width={247}
+          height={206}
+          style={{ transform: `translateX(${scrollPosition + startScrool}px)` }}
+        />
+        <Image
+          className={styles.folder}
+          src="/archive/2023.png"
+          alt="thing"
+          width={247}
+          height={206}
+          style={{ transform: `translateX(${scrollPosition + startScrool}px)` }}
+        />
+        <Image
+          className={styles.folder}
+          src="/archive/2023.png"
+          alt="thing"
+          width={247}
+          height={206}
+          style={{ transform: `translateX(${scrollPosition + startScrool}px)` }}
+        />
+        <Image
+          className={styles.folder}
+          src="/archive/2023.png"
+          alt="thing"
+          width={247}
+          height={206}
+          style={{ transform: `translateX(${scrollPosition + startScrool}px)` }}
+        />
+        <Image
+          className={styles.folder}
+          src="/archive/2023.png"
+          alt="thing"
+          width={247}
+          height={206}
+          style={{ transform: `translateX(${scrollPosition + startScrool}px)` }}
+        />
+        <Image
+          className={styles.folder}
+          src="/archive/2023.png"
+          alt="thing"
+          width={247}
+          height={206}
+          style={{ transform: `translateX(${scrollPosition + startScrool}px)` }}
+        />
+      </div>
+
+      <div className={styles.bar}>
+        <div
+          className={styles.progress}
+          style={{ transform: `translateX(${scrollPosition * -1}px)` }}
+        ></div>
+      </div>
+    </div>
+  );
 }
