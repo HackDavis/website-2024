e89616b5ce5ef4a541f0520b4ba47df8348c02a8
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2024-01-27 14:58:32 -0800

    feat: current progress

--- app/(pages)/about-us/_components/Archive/Archive.tsx
+++ app/(pages)/about-us/_components/Archive/Archive.tsx
@@ -1,67 +1,177 @@
 'use client';
 import styles from './Archive.module.scss';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+type arrowColors = {
+  circleStroke: string;
+  circleFill: string;
+  pathStroke: string;
+};
+
+const allArrowColors = {
+  gray: {
+    circleStroke: '#B3B3B3',
+    circleFill: '#DBDBDB',
+    pathStroke: '#929292',
+  },
+  blue: {
+    circleStroke: '#88D8DD',
+    circleFill: '#9EE7E5',
+    pathStroke: '#005271',
+  },
+};
 
 export default function Archive() {
+  // this represents how much we have moved our folders and scroll bar across the page
+  // starts at 0 because at the beggining, we haven't clicked on any arrows to move the elements around
   const [scrollPosition, setScrollPosition] = useState(0);
 
-  const moveLeft = () => {
-    setScrollPosition(scrollPosition - 127.57); // max is 893
+  const [leftArrowColor, setLeftArrowColor] = useState(allArrowColors.gray);
+  const [rightArrowColor, setRightArrowColor] = useState(allArrowColors.blue);
+
+  let viewportWidth = 0;
+
+  if (typeof window !== 'undefined') {
+    viewportWidth = window.innerWidth;
+  }
+
+  // This is used to calc how much to move the elements when scrolling,
+  // was orginally 127.5 as a fixed pixel value
+  // 75% of the vw is how long the progress bar is
+  // 127.5 was 12% of the progress bar, so find 12% of it
+  let pixelValue = (75 * viewportWidth) / 100;
+  pixelValue = (12 * pixelValue) / 100;
+
+  // each element will move by this amount each time an arrow is hit
+  const scrollDistance = pixelValue; // was 127.57
+
+  // adjustment thats added to get the folders at the right horizontal alignment at the start
+  const startScroll = 0;
+
+  // this is the furthest left we can move the folders
+  // based off the math it was 62% of vw
+  const pixelValue2 = (62 * viewportWidth) / 100;
+  const mostLeftScrollPosition = -pixelValue2;
+
+  // this is the furthest right we can move the folders (starts like this)
+  const mostRightScrollPosition = 0;
+
+  // this useEffect checks to see if we need to change the colors of the buttons
+  useEffect(() => {
+    // if we're at the start, the left arrow should be grayed out
+    if (scrollPosition == 0) {
+      setLeftArrowColor(allArrowColors.gray);
+    } else {
+      setLeftArrowColor(allArrowColors.blue);
+    }
+
+    // if we're at the end, the right arrow should be grayed out
+    if (scrollPosition == mostLeftScrollPosition) {
+      setRightArrowColor(allArrowColors.gray);
+    } else {
+      setRightArrowColor(allArrowColors.blue);
+    }
+  }, [scrollPosition, mostLeftScrollPosition]);
+
+  // used to make the folders move left when the user hits RIGHT arrow
+  const moveItemsLeft = () => {
+    setScrollPosition(
+      Math.max(mostLeftScrollPosition, scrollPosition - scrollDistance)
+    );
   };
 
-  const startScrool = 290;
+  // used to make the folders move right when the user hits LEFT arrow
+  const moveItemsRight = () => {
+    setScrollPosition(
+      Math.min(mostRightScrollPosition, scrollPosition + scrollDistance)
+    );
+  };
 
+  /*
+  ****IMPORTANT****
+    These functions that deal with translation are directly tied to the folders.
+    The same math is used for the movement of the progress bar, but it's inversed(*-1) because
+    the progress bar moves in the opposite direction of the folders
+  ****IMPORTANT****
+  */
+
+  // alert(scrollPosition);
   return (
-    <div className={styles.container} onClick={moveLeft}>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.topLine}>
+        <p className={styles.text}>From the archive</p>
+        <div className={styles.arrowContainer}>
+          <div onClick={moveItemsRight}>
+            <LeftArrow arrowColors={leftArrowColor} />
+          </div>
+
+          <div onClick={moveItemsLeft}>
+            <RightArrow arrowColors={rightArrowColor} />
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.carousel}>
+        <div>
+          <Image
+            className={styles.folder}
+            src="/archive/2023.png"
+            alt="thing"
+            width={247}
+            height={206}
+            style={{
+              transform: `translateX(${scrollPosition + startScroll}px)`,
+            }}
+          />
+        </div>
         <Image
           className={styles.folder}
-          src="/archive/2023.png"
+          src="/archive/2022.png"
           alt="thing"
           width={247}
           height={206}
-          style={{ transform: `translateX(${scrollPosition + startScrool}px)` }}
+          style={{ transform: `translateX(${scrollPosition + startScroll}px)` }}
         />
         <Image
           className={styles.folder}
-          src="/archive/2023.png"
+          src="/archive/2021.png"
           alt="thing"
           width={247}
           height={206}
-          style={{ transform: `translateX(${scrollPosition + startScrool}px)` }}
+          style={{ transform: `translateX(${scrollPosition + startScroll}px)` }}
         />
         <Image
           className={styles.folder}
-          src="/archive/2023.png"
+          src="/archive/2020.png"
           alt="thing"
           width={247}
           height={206}
-          style={{ transform: `translateX(${scrollPosition + startScrool}px)` }}
+          style={{ transform: `translateX(${scrollPosition + startScroll}px)` }}
         />
         <Image
           className={styles.folder}
-          src="/archive/2023.png"
+          src="/archive/2019.png"
           alt="thing"
           width={247}
           height={206}
-          style={{ transform: `translateX(${scrollPosition + startScrool}px)` }}
+          style={{ transform: `translateX(${scrollPosition + startScroll}px)` }}
         />
         <Image
           className={styles.folder}
-          src="/archive/2023.png"
+          src="/archive/2018.png"
           alt="thing"
           width={247}
           height={206}
-          style={{ transform: `translateX(${scrollPosition + startScrool}px)` }}
+          style={{ transform: `translateX(${scrollPosition + startScroll}px)` }}
         />
         <Image
           className={styles.folder}
-          src="/archive/2023.png"
+          src="/archive/2017.png"
           alt="thing"
           width={247}
           height={206}
-          style={{ transform: `translateX(${scrollPosition + startScrool}px)` }}
+          style={{ transform: `translateX(${scrollPosition + startScroll}px)` }}
         />
       </div>
 
@@ -71,6 +181,65 @@ export default function Archive() {
           style={{ transform: `translateX(${scrollPosition * -1}px)` }}
         ></div>
       </div>
+    </div>
+  );
+}
+
+function LeftArrow(props: { arrowColors: arrowColors }) {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="48"
+        height="48"
+        viewBox="0 0 34 34"
+        fill="none"
+      >
+        <circle
+          cx="17"
+          cy="17"
+          r="16.5"
+          fill={props.arrowColors.circleFill}
+          stroke={props.arrowColors.circleStroke}
+        />
+        <path
+          d="M19 12L14 16.2857L19 21"
+          stroke={props.arrowColors.pathStroke}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
+  );
+}
+
+function RightArrow(props: { arrowColors: arrowColors }) {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="48"
+        height="48"
+        viewBox="0 0 34 35"
+        fill="none"
+      >
+        <circle
+          cx="17"
+          cy="17"
+          r="16.5"
+          transform="matrix(-1 0 0 1 34 0.0136719)"
+          fill={props.arrowColors.circleFill}
+          stroke={props.arrowColors.circleStroke}
+        />
+        <path
+          d="M15 12.0137L20 16.2994L15 21.0137"
+          stroke={props.arrowColors.pathStroke}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
     </div>
   );
 }
