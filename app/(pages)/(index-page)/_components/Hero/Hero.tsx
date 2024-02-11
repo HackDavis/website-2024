0a3eb887ac3eb4431ec1bb9802e72d03afb04f72
Win Cheng <winzamark12@gmail.com>
2024-02-11 14:36:05 -0800

    Hero Init

--- app/(pages)/(index-page)/_components/Hero/Hero.tsx
+++ app/(pages)/(index-page)/_components/Hero/Hero.tsx
@@ -5,16 +5,61 @@ import styles from './Hero.module.scss';
 export default function Hero() {
   return (
     <div className={styles.container}>
-      <div className={styles.img_container}>
+      <div className={styles.clouds}>
         <Image
-          src="/index/city.jpg"
+          src="/index/Hero/hero_left-backcloud.png"
           alt="serene forest scene in the night"
           fill
-          style={{ objectFit: 'cover', opacity: '0.6' }}
+          className={styles.clouds_image}
+          z-index={0}
+        />
+        <Image
+          src="/index/Hero/hero_right-backcloud.png"
+          alt="serene forest scene in the night"
+          fill
+          className={styles.clouds_image}
+          z-index={0}
+        />
+        <Image
+          src="/index/Hero/hero_left-cloud.png"
+          alt="serene forest scene in the night"
+          fill
+          className={styles.clouds_image}
+          z-index={1}
+        />
+        <Image
+          src="/index/Hero/hero_right-cloud.png"
+          alt="serene forest scene in the night"
+          fill
+          className={styles.clouds_image}
+          z-index={1}
         />
       </div>
-      <div className={styles.welcome}>
-        <h1>PLATFORM TEAM NEXT.JS TEMPLATE</h1>
+      <div className={styles.grass}>
+        <Image
+          src="/index/Hero/hero_far-grass.png"
+          alt="serene forest scene in the night"
+          fill
+          className={styles.grass_image}
+          z-index={2}
+          style={{ border: '2px solid red', marginTop: '-10%' }}
+        />
+        <Image
+          src="/index/Hero/hero_mid-grass.png"
+          alt="serene forest scene in the night"
+          fill
+          className={styles.grass_image}
+          z-index={3}
+          style={{ border: '2px solid blue' }}
+        />
+        <Image
+          src="/index/Hero/hero_front-grass.png"
+          alt="serene forest scene in the night"
+          fill
+          className={styles.grass_image}
+          z-index={4}
+          style={{ border: '2px solid black', marginTop: '14%' }}
+        />
       </div>
     </div>
   );
