bc2cba22e00924cabb4a1a9b153df10d299332bd
Sauvikesh <saulal@ucdavis.edu>
2024-02-11 13:57:05 -0800

    added images, positioned log rocks

---
+++ app/(pages)/(index-page)/_components/Hero/WhatIsHackdavis/WhatIsHackdavis.tsx
@@ -0,0 +1,32 @@
+import styles from './WhatIsHackdavis.module.scss';
+import Image from 'next/image';
+
+export default function WhatIsHackdavis() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logSection}>
+        <div>
+          <Image
+            src="/index/whatIsHackdavis/log_left-rock.png"
+            alt="serene forest scene in the night"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className={styles.logLeft} // optional
+          />
+        </div>
+
+        <div>
+          <Image
+            src="/index/whatIsHackdavis/log_right-rock.png"
+            alt="serene forest scene in the night"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className={styles.logRight} // optional
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/(pages)/(index-page)/page.tsx
+++ app/(pages)/(index-page)/page.tsx
@@ -1,9 +1,10 @@
 import Hero from './_components/Hero/Hero';
+import WhatIsHackdavis from './_components/Hero/WhatIsHackdavis/WhatIsHackdavis';
 
 export default function Home() {
   return (
     <main>
-      <Hero />
+      <WhatIsHackdavis />
     </main>
   );
 }
