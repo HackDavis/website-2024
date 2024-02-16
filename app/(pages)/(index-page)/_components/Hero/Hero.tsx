ccd6028ccbb8738eb4b87364c870cb6174525f7a
Win Cheng <winzamark12@gmail.com>
2024-02-15 18:04:41 -0800

    Adjusted Size

--- app/(pages)/(index-page)/_components/Hero/Hero.tsx
+++ app/(pages)/(index-page)/_components/Hero/Hero.tsx
@@ -54,24 +54,24 @@ export default function Hero() {
         />
       </div>
       <div className={styles.characters}>
-        <div className={styles.characters_duck}>
+        <div className={styles.characters_rabbit}>
           <Image
             src="/index/Hero/hero_rabbit.png"
             alt="serene forest scene in the night"
             fill
-            className={styles.characters_duck_image}
+            className={styles.characters_rabbit_image}
             z-index={4}
             style={{
               border: '2px solid black',
             }}
           />
         </div>
-        <div className={styles.characters_rabbit}>
+        <div className={styles.characters_duck}>
           <Image
             src="/index/Hero/hero_duck.png"
             alt="serene forest scene in the night"
             fill
-            className={styles.characters_rabbit_image}
+            className={styles.characters_duck_image}
             z-index={4}
             style={{ border: '2px solid black' }}
           />
