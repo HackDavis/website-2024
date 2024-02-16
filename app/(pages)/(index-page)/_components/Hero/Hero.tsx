6087d76e7efac09ccf4d35a125a48e82a0500def
Win Cheng <winzamark12@gmail.com>
2024-02-15 16:21:09 -0800

    Added Rabbit and Bunny

--- app/(pages)/(index-page)/_components/Hero/Hero.tsx
+++ app/(pages)/(index-page)/_components/Hero/Hero.tsx
@@ -42,7 +42,7 @@ export default function Hero() {
           fill
           className={styles.grass_image}
           z-index={2}
-          style={{ border: '2px solid red', marginTop: '-10%' }}
+          style={{ border: '2px solid red', marginTop: '-23%' }}
         />
         <Image
           src="/index/Hero/hero_mid-grass.png"
@@ -50,17 +50,41 @@ export default function Hero() {
           fill
           className={styles.grass_image}
           z-index={3}
-          style={{ border: '2px solid blue' }}
-        />
-        <Image
-          src="/index/Hero/hero_front-grass.png"
-          alt="serene forest scene in the night"
-          fill
-          className={styles.grass_image}
-          z-index={4}
-          style={{ border: '2px solid black', marginTop: '14%' }}
+          style={{ border: '2px solid blue', marginTop: '-12%' }}
         />
       </div>
+      <div className={styles.characters}>
+        <div className={styles.characters_duck}>
+          <Image
+            src="/index/Hero/hero_rabbit.png"
+            alt="serene forest scene in the night"
+            fill
+            className={styles.characters_duck_image}
+            z-index={4}
+            style={{
+              border: '2px solid black',
+            }}
+          />
+        </div>
+        <div className={styles.characters_rabbit}>
+          <Image
+            src="/index/Hero/hero_duck.png"
+            alt="serene forest scene in the night"
+            fill
+            className={styles.characters_rabbit_image}
+            z-index={4}
+            style={{ border: '2px solid black' }}
+          />
+        </div>
+      </div>
+      {/* <Image
+        src="/index/Hero/hero_front-grass.png"
+        alt="serene forest scene in the night"
+        fill
+        className={styles.grass_image}
+        z-index={4}
+        style={{ border: '2px solid black', marginTop: '0' }}
+      /> */}
     </div>
   );
 }
