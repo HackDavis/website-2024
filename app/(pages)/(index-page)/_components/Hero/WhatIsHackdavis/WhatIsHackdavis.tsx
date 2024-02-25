406728faceafe3a62a1f21240a968b46195759c3
Sauvikesh <saulal@ucdavis.edu>
2024-02-24 20:12:19 -0800

    finished log :00

--- app/(pages)/(index-page)/_components/Hero/WhatIsHackdavis/WhatIsHackdavis.tsx
+++ app/(pages)/(index-page)/_components/Hero/WhatIsHackdavis/WhatIsHackdavis.tsx
@@ -4,126 +4,104 @@ import Image from 'next/image';
 export default function WhatIsHackdavis() {
   return (
     <div className={styles.container}>
-      <div className={styles.logSection}>
-        <div>
-          <Image
-            src="/index/whatIsHackdavis/log_layer0.png"
-            alt="serene forest scene in the night"
-            width={0}
-            height={0}
-            sizes="100vw"
-            priority
-            className={styles.logLayer0}
-          />
-        </div>
-
-        <div>
-          <Image
-            src="/index/whatIsHackdavis/log_layer1-left.png"
-            alt="serene forest scene in the night"
-            width={0}
-            height={0}
-            sizes="100vw"
-            className={styles.logLayer1Left}
-          />
-        </div>
-
-        <div>
-          <Image
-            src="/index/whatIsHackdavis/log_layer1-right.png"
-            alt="serene forest scene in the night"
-            width={0}
-            height={0}
-            sizes="100vw"
-            className={styles.logLayer1Right}
-          />
-        </div>
-
-        <div>
-          <Image
-            src="/index/whatIsHackdavis/log_layer2-left.png"
-            alt="serene forest scene in the night"
-            width={0}
-            height={0}
-            sizes="100vw"
-            className={styles.logLayer2Left}
-          />
-        </div>
-
-        <div>
-          <Image
-            src="/index/whatIsHackdavis/log_layer2-right.png"
-            alt="serene forest scene in the night"
-            width={0}
-            height={0}
-            sizes="100vw"
-            className={styles.logLayer2Right}
-          />
-        </div>
-
-        <div>
-          <Image
-            src="/index/whatIsHackdavis/log_gingko.png"
-            alt="serene forest scene in the night"
-            width={0}
-            height={0}
-            sizes="100vw"
-            className={styles.logGingko}
-          />
-        </div>
-
-        <div>
+      <div className={styles.text}>
+        <h1 className={styles.heading}>What is Hackdavis?</h1>
+        <p className={styles.paragraph}>
+          HackDavis is the largest collegiate hackathon in California where over
+          750 students, creators, and leaders come together for 36 hours to
+          create for social good.
+        </p>
+      </div>
+      <div className={styles.logStuff}>
+        <div className={styles.logLeftSection}>
           <Image
             src="/index/whatIsHackdavis/log_left-rock.png"
             alt="serene forest scene in the night"
-            width={0}
-            height={0}
-            sizes="100vw"
+            width={4000}
+            height={100}
             className={styles.logLeft}
           />
         </div>
 
-        <div>
+        <div className={styles.logRightSection}>
+          <Image
+            src="/index/whatIsHackdavis/log_layer0.png"
+            alt="serene forest scene in the night"
+            width={4000}
+            height={1000}
+            className={styles.logLayer0}
+          />
+
+          <Image
+            src="/index/whatIsHackdavis/log_layer1-left.png"
+            alt="serene forest scene in the night"
+            width={4000}
+            height={1000}
+            className={styles.logLayer1Left}
+          />
+
+          <Image
+            src="/index/whatIsHackdavis/log_layer2-left.png"
+            alt="serene forest scene in the night"
+            width={4000}
+            height={100}
+            className={styles.logLayer2Left}
+          />
+
           <Image
             src="/index/whatIsHackdavis/log_right-rock.png"
             alt="serene forest scene in the night"
-            width={0}
-            height={0}
-            sizes="100vw"
+            width={4000}
+            height={100}
             className={styles.logRight}
           />
-        </div>
 
-        <div>
           <Image
-            src="/index/whatIsHackdavis/log_water-sheen.png"
+            src="/index/whatIsHackdavis/log_layer1-right.png"
             alt="serene forest scene in the night"
-            width={0}
-            height={0}
-            sizes="100vw"
-            className={styles.logWaterSheen}
+            width={4000}
+            height={100}
+            className={styles.logLayer1Right}
           />
-        </div>
 
-        <div>
+          <Image
+            src="/index/whatIsHackdavis/log_layer2-right.png"
+            alt="serene forest scene in the night"
+            width={4000}
+            height={100}
+            className={styles.logLayer2Right}
+          />
+
           <Image
             src="/index/whatIsHackdavis/log_rabbit.png"
             alt="serene forest scene in the night"
-            width={0}
-            height={0}
-            sizes="100vw"
+            width={4000}
+            height={100}
             className={styles.logRabbit}
           />
-        </div>
 
-        <div>
           <Image
             src="/index/whatIsHackdavis/log_frog.png"
             alt="serene forest scene in the night"
-            width={0}
-            height={0}
-            sizes="100vw"
+            width={4000}
+            height={100}
             className={styles.logFrog}
+          />
+
+          <Image
+            src="/index/whatIsHackdavis/log_gingko.png"
+            alt="serene forest scene in the night"
+            width={4000}
+            height={100}
+            className={styles.logGingko}
+          />
+
+          <Image
+            src="/index/whatIsHackdavis/log_water-sheen.png"
+            alt="serene forest scene in the night"
+            width={4000}
+            height={100}
+            className={styles.logWaterSheen}
           />
         </div>
       </div>
