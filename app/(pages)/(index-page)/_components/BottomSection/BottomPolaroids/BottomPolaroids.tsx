310b1e9468c69b70d4ed8eaf4e88ff54772106f2
Sauvikesh <saulal@ucdavis.edu>
2024-02-27 16:32:44 -0800

    added new polaroid images, adjusted positioning on mobile view

--- app/(pages)/(index-page)/_components/BottomSection/BottomPolaroids/BottomPolaroids.tsx
+++ app/(pages)/(index-page)/_components/BottomSection/BottomPolaroids/BottomPolaroids.tsx
@@ -6,21 +6,21 @@ export default function BottomPolaroids() {
     <div className={styles.container}>
       <div className={styles.allPolaroids}>
         <Image
-          src="/index/Polaroids/middlePolaroid.png"
+          src="/index/Polaroids/final_Footer-img3.png"
           alt="serene forest scene in the night"
           width={100}
           height={100}
           className={styles.backPolaroid}
         />
         <Image
-          src="/index/Polaroids/middlePolaroid.png"
+          src="/index/Polaroids/final_Footer-img2.png"
           alt="serene forest scene in the night"
           width={100}
           height={100}
           className={styles.middlePolaroid}
         />
         <Image
-          src="/index/Polaroids/middlePolaroid.png"
+          src="/index/Polaroids/final_Footer-img1.png"
           alt="serene forest scene in the night"
           width={100}
           height={100}
