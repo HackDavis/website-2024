8be3f5c71c1d659c2e500ac2fcba2da9ee810c73
Win Cheng <winzamark12@gmail.com>
2024-02-27 21:45:47 -0800

    Working on responsiveness on phone size

--- app/(pages)/(index-page)/_components/Landing/Landing.tsx
+++ app/(pages)/(index-page)/_components/Landing/Landing.tsx
@@ -1,19 +1,21 @@
 import Clouds from '../Clouds/Clouds';
 import Form from '../Form/Form';
 import Grass from '../Grass/Grass';
-import styles from './landing.module.scss';
+import styles from './Landing.module.scss';
 
 export default function Landing() {
   return (
     <main>
-      <div className={styles.clouds}>
-        <Clouds />
-      </div>
-      <div className={styles.grass}>
-        <Grass />
-      </div>
-      <div className={styles.form}>
-        <Form />
+      <div className={styles.landing_container}>
+        <div className={styles.form}>
+          <Form />
+        </div>
+        <div className={styles.clouds}>
+          <Clouds />
+        </div>
+        <div className={styles.grass}>
+          <Grass />
+        </div>
       </div>
     </main>
   );
