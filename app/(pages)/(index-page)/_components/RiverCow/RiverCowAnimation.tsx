5499a7c034ff07cb8a2d19125f69161713353c4a
Austin Shih <austinhshih@gmail.com>
2024-03-12 14:48:12 -0700

    Update RiverCowAnimation.tsx

--- app/(pages)/(index-page)/_components/RiverCow/RiverCowAnimation.tsx
+++ app/(pages)/(index-page)/_components/RiverCow/RiverCowAnimation.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 import styles from './RiverCowAnimation.module.scss';
