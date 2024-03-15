83e94262eda5a0fcca8f51c3d4e55fae02022072
Sauvikesh <saulal@ucdavis.edu>
2024-03-14 23:04:24 -0700

    refactor: displaying team filter buttons using map function to reduce clutter

--- app/(pages)/about-us/_components/OurTeam/OurTeam.tsx
+++ app/(pages)/about-us/_components/OurTeam/OurTeam.tsx
@@ -42,6 +42,15 @@ export default function OurTeam() {
     loadInitialTeam();
   }, []);
 
+  const teamNames = [
+    'Design',
+    'Technical',
+    'Marketing',
+    'Sponsorship/Finance',
+    'External',
+    'Operations',
+  ];
+
   return (
     <div className={styles.ourTeam}>
       <div className={styles.ourTeam_container}>
@@ -63,74 +72,21 @@ export default function OurTeam() {
             <p>2024</p>
             <FontAwesomeIcon icon={faAngleDown} size="1x" />
           </div> */}
-            <div
-              className={`${
-                styles.ourTeam_container_embla_filterButtons_button
-              } ${activeTeam === 'Design' ? styles.active : ''}`}
-              onClick={async () => {
-                setActiveTeam('Design');
-                setTeamMembers(await filterTeamMongo('Design', 2024));
-              }}
-            >
-              <p>Design</p>
-            </div>
-            <div
-              className={`${
-                styles.ourTeam_container_embla_filterButtons_button
-              } ${activeTeam === 'Technical' ? styles.active : ''}`}
-              onClick={async () => {
-                setActiveTeam('Technical');
-                setTeamMembers(await filterTeamMongo('Technical', 2024));
-              }}
-            >
-              <p>Technical</p>
-            </div>
-            <div
-              className={`${
-                styles.ourTeam_container_embla_filterButtons_button
-              } ${activeTeam === 'Marketing' ? styles.active : ''}`}
-              onClick={async () => {
-                setActiveTeam('Marketing');
-                setTeamMembers(await filterTeamMongo('Marketing', 2024));
-              }}
-            >
-              <p>Marketing</p>
-            </div>
-            <div
-              className={`${
-                styles.ourTeam_container_embla_filterButtons_button
-              } ${activeTeam === 'Sponsorship/Finance' ? styles.active : ''}`}
-              onClick={async () => {
-                setActiveTeam('Sponsorship/Finance');
-                setTeamMembers(
-                  await filterTeamMongo('Sponsorship/Finance', 2024)
-                );
-              }}
-            >
-              <p>Sponsorship + Finance</p>
-            </div>
-            <div
-              className={`${
-                styles.ourTeam_container_embla_filterButtons_button
-              } ${activeTeam === 'External' ? styles.active : ''}`}
-              onClick={async () => {
-                setActiveTeam('External');
-                setTeamMembers(await filterTeamMongo('External', 2024));
-              }}
-            >
-              <p>External</p>
-            </div>
-            <div
-              className={`${
-                styles.ourTeam_container_embla_filterButtons_button
-              } ${activeTeam === 'Operations' ? styles.active : ''}`}
-              onClick={async () => {
-                setActiveTeam('Operations');
-                setTeamMembers(await filterTeamMongo('Operations', 2024));
-              }}
-            >
-              <p>Operations</p>
-            </div>
+            {teamNames.map((teamName, index) => (
+              <div key={index}>
+                <div
+                  className={`${
+                    styles.ourTeam_container_embla_filterButtons_button
+                  } ${activeTeam === teamName ? styles.active : ''}`}
+                  onClick={async () => {
+                    setActiveTeam(teamName);
+                    setTeamMembers(await filterTeamMongo(teamName, 2024));
+                  }}
+                >
+                  <p>{teamName}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
 
