292d098ee1b0bc3d50e49ed90a50e2c0e5a950fb
Win Cheng <winzamark12@gmail.com>
2024-04-10 10:23:12 -0700

    Color Coordinated based on type of activities

--- app/(pages)/(index-page)/event/_components/Schedule/_components/TimeTable.tsx
+++ app/(pages)/(index-page)/event/_components/Schedule/_components/TimeTable.tsx
@@ -5,6 +5,12 @@ interface TimeTableProps {
   timeChunks: TimeChunk[];
 }
 
+const colorActivities: Record<string, string> = {
+  Activity: '#60A5FA', // Blue
+  Menu: '#34D399', // Green
+  Workshop: '#FBBF24', // Yellow
+};
+
 export default function TimeTable({ timeChunks }: TimeTableProps) {
   return (
     <main className="">
@@ -19,7 +25,6 @@ export default function TimeTable({ timeChunks }: TimeTableProps) {
             <div
               className={`tw-grid tw-gap-3 tw-border tw-border-purple-300`}
               style={{
-                // gridTemplateColumns: `repeat(${timeChunk.eventBlocks.length}, minmax(0, 1fr))`,
                 gridTemplateColumns: 'auto',
               }}
             >
@@ -27,7 +32,10 @@ export default function TimeTable({ timeChunks }: TimeTableProps) {
                 <div
                   key={event.title}
                   className="tw-flex tw-flex-col tw-rounded-3xl tw-border tw-border-black tw-bg-gray-400 tw-p-3"
-                  style={calcRow(event, timeChunk.startTime)}
+                  style={{
+                    ...calcRow(event, timeChunk.startTime),
+                    backgroundColor: colorActivities[event.type],
+                  }}
                 >
                   <span>Event #{event_index + 1}</span>
                   <span>{event.title}</span>
