52bad6e593d06db5bea62e706fc868247b13de0e
laasya <laasya.m1@gmail.com>
2024-04-21 16:23:20 -0700

    updates to timer

--- app/(pages)/(index-page)/event/page.tsx
+++ app/(pages)/(index-page)/event/page.tsx
@@ -1,22 +1,23 @@
 'use client';
 import { LoadingProvider } from '@contexts/LoadingContext';
 import Loader from '@components/Loader/Loader';
-import Timer from './_components/Timer/Timer';
+//import Timer from './_components/LandingEvent/_components/Timer/Timer';
+//import Landing from '../_components/Landing/Landing';
 import WhatIsHackdavis from '../_components/WhatIsHackdavis/WhatIsHackdavis';
-import ScheduleBlock from './_components/Schedule/ScheduleBlock';
+import ScheduleBlock from './_components/Schedule/Schedule';
 import PrizeList from './_components/Prizes/PrizeList';
 import RiverCow from '../_components/RiverCow/RiverCow';
 import BottomSection from '../_components/BottomSection/BottomSection';
 import Sponsors from '../_components/Sponsors/Sponsors';
-//import Landing from '../_components/Landing/Landing';
+import LandingEvent from './_components/LandingEvent/LandingEvent';
 
 export default function Home() {
   return (
     <LoadingProvider>
       <Loader />
       <main style={{ backgroundColor: 'white', scrollBehavior: 'smooth' }}>
-        <div id="timer">
-          <Timer />
+        <div id="landing">
+          <LandingEvent />
         </div>
         <div id="schedule">
           <ScheduleBlock />
