724dab3d3af8a4f13e0ff1b5d4a8fc5e9ef734a8
laasya <laasya.m1@gmail.com>
2024-04-21 17:24:57 -0700

    timer updates

---
+++ app/(pages)/(index-page)/event/_components/LandingEvent/LandingEvent.tsx
@@ -0,0 +1,23 @@
+import Clouds from '@landingComponents/Clouds/Clouds';
+import Grass from '@landingComponents/Grass/Grass';
+import styles from './LandingEvent.module.scss';
+
+import Timer from './_components/Timer/Timer';
+
+export default function LandingEvent() {
+  return (
+    <main className={styles.landing}>
+      <div className={styles.landing_container}>
+        <div className={styles.form}>
+          <Timer />
+        </div>
+        <div className={styles.clouds}>
+          <Clouds />
+        </div>
+        <div className={styles.grass}>
+          <Grass />
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ app/(pages)/(index-page)/event/_components/LandingEvent/_components/Timer/Timer.tsx
@@ -0,0 +1,45 @@
+import styles from './Timer.module.scss';
+
+//import { CiCalendar } from 'react-icons/ci';
+import Link from 'next/link';
+
+type FormLink = {
+  title: string;
+  url: string;
+};
+
+const FormLinks: { [key: string]: FormLink } = {
+  Register: {
+    title: 'Submit Project',
+    url: 'https://form.typeform.com/to/RvoDiujh?utm_source=WEBSITE',
+  },
+  Sponsor: { title: 'Get Help', url: 'mailto:team@hackdavis.io' },
+  Calendar: {
+    title: 'HackDavis 2024 Calendar',
+    url: 'https://drive.google.com/file/d/1AGBLnS55qNEuGNXh9VLSdx2PaHsGVgE7/view?usp=drive_link',
+  },
+};
+
+export default function Form() {
+  return (
+    <div className={styles.background}>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <img src="/navbar/logo.svg" alt="Logo" className={styles.logo} />{' '}
+          <span className={styles.title_social}>HackDavis 2024</span>
+        </div>
+        <div className={styles.buttons}>
+          <Link
+            href={FormLinks.Register.url}
+            className={styles.buttons_register}
+          >
+            <p>{FormLinks.Register.title}</p>
+          </Link>
+          <a href={FormLinks.Sponsor.url} className={styles.buttons_sponsor}>
+            <p>{FormLinks.Sponsor.title}</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
