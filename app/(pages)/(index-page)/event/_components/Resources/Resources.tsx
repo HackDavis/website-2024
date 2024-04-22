73b7999c9a444b6cec0a26d8d3397283d104b2f5
Win Cheng <winzamark12@gmail.com>
2024-04-22 14:22:51 -0700

    Resource Columns based on event or index, working on responsiveness

--- app/(pages)/(index-page)/event/_components/Resources/Resources.tsx
+++ app/(pages)/(index-page)/event/_components/Resources/Resources.tsx
@@ -11,7 +11,7 @@ export default function Resources({
   resourcePack: ResourceType[];
 }) {
   const numColumns = resourcePack.length;
-  // const gridTemplateColumns = `repeat(${numColumns}, 1fr)`;
+  const gridTemplateColumns = `repeat(${numColumns}, 1fr)`;
 
   return (
     <main className={styles.main_container}>
@@ -19,15 +19,15 @@ export default function Resources({
       <div
         className="tw-grid tw-gap-6"
         style={{
-          // gridTemplateColumns,
-          gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
+          gridTemplateColumns,
+          // gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
         }}
       >
         {resourcePack.map((resource, index) => {
           return (
             <div
               key={index}
-              className="tw-text-dark-blue sm:tw-aspect-content tw-flex tw-cursor-pointer tw-flex-col tw-items-center tw-justify-center tw-gap-6 tw-rounded-3xl tw-border tw-p-9 md:tw-aspect-auto lg:tw-aspect-auto"
+              className="tw-text-dark-blue sm:tw-aspect-content tw-flex tw-min-w-0 tw-cursor-pointer tw-flex-col tw-items-center tw-justify-center tw-gap-6 tw-rounded-3xl tw-border tw-p-9 md:tw-aspect-auto lg:tw-aspect-auto"
             >
               <div className={styles.card_icons}>{resource.icon}</div>
               <h4 className={styles.card_titles}>{resource.title}</h4>
