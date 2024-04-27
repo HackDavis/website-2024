690795fc0dd22aa6ef320a18e17e636dea1e904b
Win Cheng <winzamark12@gmail.com>
2024-04-27 11:54:12 -0700

    Wifi up

--- app/(pages)/(index-page)/event/dayTimer/page.tsx
+++ app/(pages)/(index-page)/event/dayTimer/page.tsx
@@ -9,6 +9,16 @@ export default function DayTimer() {
       <div id="landing">
         <LandingEvent />
       </div>
+      <div
+        className="tw-absolute tw-left-1/2 tw-z-40 tw-flex tw-gap-8 tw-font-semibold"
+        style={{
+          top: '65%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        <span>WIFI: hackdavis2024</span>
+        <span>Password: intel@HD2024</span>
+      </div>
     </main>
   );
 }
