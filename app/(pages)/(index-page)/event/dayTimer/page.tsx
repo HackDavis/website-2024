473026da7e13685c2c2c44ffbb4c09c2018777b4
Sauvikesh <saulal@ucdavis.edu>
2024-04-27 11:19:20 -0700

    FIX TO TIMER AGAIN

--- app/(pages)/(index-page)/event/dayTimer/page.tsx
+++ app/(pages)/(index-page)/event/dayTimer/page.tsx
@@ -5,6 +5,7 @@ import LandingEvent from '../_components/LandingEvent/LandingEvent';
 export default function DayTimer() {
   return (
     <main style={{ backgroundColor: 'white', scrollBehavior: 'smooth' }}>
+      <div style={{ height: '150px', background: '#7ACCDA' }}></div>
       <div id="landing">
         <LandingEvent />
       </div>
