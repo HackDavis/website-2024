1e9a31b3dce801335fff15e7e0a73be2bf2e0fa8
Win Cheng <winzamark12@gmail.com>
2024-06-14 15:03:11 -0700

    image added, form and responsiveness left

--- app/(pages)/(index-page)/_components/Landing/Landing.tsx
+++ app/(pages)/(index-page)/_components/Landing/Landing.tsx
@@ -1,21 +1,17 @@
-import Clouds from './_components/Clouds/Clouds';
 import Form from './_components/Form/Form';
-import Grass from './_components/Grass/Grass';
-import styles from './Landing.module.scss';
+import Image from 'next/image';
+import PostDoeIMG from '@/public/postDOE/charstackbf.png';
 
 export default function Landing() {
   return (
-    <main className="">
-      <div className={styles.landing_container}>
-        <div className="tw-absolute tw-left-20 tw-top-1/4 tw-z-10">
-          <Form />
-        </div>
-        <div className={styles.clouds}>
-          <Clouds />
-        </div>
-        <div className={styles.grass}>
-          <Grass />
-        </div>
+    <main className="tw-overflow-hidden">
+      <Image
+        className="tw-left-0 tw-top-0 tw-h-screen-4/5 tw-w-full"
+        src={PostDoeIMG}
+        alt="PostDoeIMG"
+      ></Image>
+      <div className="tw-absolute tw-left-20 tw-top-1/4 tw-z-10">
+        <Form />
       </div>
     </main>
   );
--- app/(pages)/(index-page)/page.tsx
+++ app/(pages)/(index-page)/page.tsx
@@ -1,18 +1,13 @@
 'use client';
 // import Everyone from './_components/Everyone/Everyone';
 import Landing from './_components/Landing/Landing';
-import { LoadingProvider } from '@contexts/LoadingContext';
-import Loader from '@components/Loader/Loader';
 
 export default function Home() {
   return (
-    <LoadingProvider>
-      <Loader />
-      <main style={{ backgroundColor: 'white', scrollBehavior: 'smooth' }}>
-        <div id="landing">
-          <Landing />
-        </div>
-      </main>
-    </LoadingProvider>
+    <main className="tw-overflow-hidden">
+      <div id="landing">
+        <Landing />
+      </div>
+    </main>
   );
 }
