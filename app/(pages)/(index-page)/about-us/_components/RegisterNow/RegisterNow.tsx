e7aa1f87bffc11fba265546debd5a719063a140e
Sauvikesh <sauviklal@gmail.com>
2024-08-28 13:16:19 -0700

    feat: changed sponsor carousel bg color, register now has cow on rock and different text

--- app/(pages)/(index-page)/about-us/_components/RegisterNow/RegisterNow.tsx
+++ app/(pages)/(index-page)/about-us/_components/RegisterNow/RegisterNow.tsx
@@ -4,28 +4,8 @@ import Link from 'next/link';
 export default function RegisterNow() {
   return (
     <div className={styles.container}>
-      <div className={styles.duckWithBunny}>
-        <Image
-          src="/registerNow/duck.svg"
-          alt="hackdavis Logo"
-          width={59.1}
-          height={59.1}
-          className={styles.duck}
-        />
-        <Image
-          src="/registerNow/bunny.svg"
-          alt="hackdavis Logo"
-          width={152.7}
-          height={175.4}
-          className={styles.bunny}
-        />
-      </div>
-
       <div className={styles.textContent}>
-        <h1 className={styles.text}>
-          Ready to create for{' '}
-          <span className={styles.socialGood}>social good?</span>
-        </h1>
+        <h1 className={styles.text}>Ready to create for social good?</h1>
         <Link
           target="_blank"
           href={'https://form.typeform.com/to/RvoDiujh?utm_source=WEBSITE'}
@@ -36,11 +16,11 @@ export default function RegisterNow() {
       </div>
 
       <Image
-        src="/registerNow/Froggy.png"
+        src="/index/Everyone/river_cow-on-rock.png"
         alt="hackdavis Logo"
-        width={232.115}
-        height={378.818}
-        className={styles.frog}
+        width={1000}
+        height={1000}
+        className={styles.cowSit}
       />
     </div>
   );
