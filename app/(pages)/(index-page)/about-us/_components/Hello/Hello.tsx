9fb57d8da00faa4c67f39e0ad53953476c33d620
Sauvikesh <sauviklal@gmail.com>
2024-08-28 13:56:18 -0700

    feat: replaced hero landing with clouds, and grass and white text

--- app/(pages)/(index-page)/about-us/_components/Hello/Hello.tsx
+++ app/(pages)/(index-page)/about-us/_components/Hello/Hello.tsx
@@ -6,7 +6,7 @@ const Hello = () => {
   return (
     <div className={styles.hello_container}>
       <div className={styles.title}>
-        <h1>Hello, we're HackDavis</h1>
+        <h1>HackDavis</h1>
         <p>
           Bringing together 750+ students, creators, and leaders to create for
           social good
@@ -14,11 +14,34 @@ const Hello = () => {
       </div>
       <div className={styles.banner}>
         <Image
-          src="/about-us-icon/Banner.svg"
+          src="/about-us-icon/bannerclouds1.png"
           alt="Banner"
           width={4000}
           height={100}
-          className={styles.banner_image}
+          className={styles.cloudRight}
+        />
+
+        <Image
+          src="/about-us-icon/bannerclouds2.png"
+          alt="Banner"
+          width={4000}
+          height={100}
+          className={styles.cloud2}
+        />
+
+        <Image
+          src="/index/Hero/hero_front-grass.png"
+          alt="Banner"
+          width={4000}
+          height={100}
+          className={styles.frontGrass}
+        />
+        <Image
+          src="/index/Hero/hero_mid-grass.png"
+          alt="Banner"
+          width={4000}
+          height={100}
+          className={styles.midGrass}
         />
       </div>
     </div>
